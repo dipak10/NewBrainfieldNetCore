448a9b2284cb0af63326a77aefea8a7a0197bfee
Dipak Parekh <dipak10494@yahoo.com>
2021-10-22 10:04:42 +0530

    Added UserID

---
+++ Data/Migrations/N20211022095200_AddingCustomUserIDColoumn.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace NewBrainfieldNetCore.Data.Migrations
+{
+    [Migration(20211022095200)]
+    public class N20211022095200_AddingCustomUserIDColoumn : Migration
+    {
+        public const string TableName = "AspNetUsers";
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+
+        public override void Up()
+        {
+            Alter.Table(TableName)
+                .AddColumn("UserID").AsInt32().Identity().NotNullable();
+        }
+    }
+}
