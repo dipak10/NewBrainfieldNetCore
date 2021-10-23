5fed2fa2fe5f6596dfc1d63f20fecda59d2fa4bb
Dipak Parekh <dipak10494@yahoo.com>
2021-10-23 11:58:44 +0530

    Added Apply exam Controller

---
+++ Controllers/ApplyExamController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewBrainfieldNetCore.Controllers
+{
+    public class ApplyExamController : Controller
+    {
+        public IActionResult Index(int ExamId, int OrderId)
+        {
+
+            return View();
+        }
+    }
+}
