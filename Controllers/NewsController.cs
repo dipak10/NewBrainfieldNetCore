ead26d604e7418f813fde7c8c6193732ff1880a5
Dipak Parekh <dipak10494@yahoo.com>
2021-10-26 17:24:33 +0530

    News Added

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,19 +1,23 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using NewBrainfieldNetCore.Data;
 using NewBrainfieldNetCore.Entities;
+using NewBrainfieldNetCore.Models;
 using NewBrainfieldNetCore.Services.Interfaces;
+using NewBrainfieldNetCore.Viewmodels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace NewBrainfieldNetCore.Controllers
 {
     [Route("Student/")]
-    public class BlogController : Controller
+    public class NewsController : Controller
     {
         private readonly ILogger<BlogController> _logger;
         private readonly INotyfService _notyf;
@@ -21,7 +25,7 @@ namespace NewBrainfieldNetCore.Controllers
         private readonly IHomeService _homeService;
         private readonly ApplicationContext _context;
 
-        public BlogController(ILogger<BlogController> logger, INotyfService notyf, ICommonService commonService,
+        public NewsController(ILogger<BlogController> logger, INotyfService notyf, ICommonService commonService,
             IHomeService homeService, ApplicationContext context)
         {
             _logger = logger;
@@ -30,47 +34,32 @@ namespace NewBrainfieldNetCore.Controllers
             _homeService = homeService;
             _context = context;
         }
-
-        [Route("ReadBlogs")]
+        [Route("ReadNews")]
         public async Task<IActionResult> Index()
         {
-            try
-            {
-                List<tblBlogs> blogs = await _context.tblBlogs.ToListAsync();
-
-                return View(blogs);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError($"Error occured while getting blogs {e.InnerException}");
-
-                return RedirectToAction("Error", "Home");
-            }
-            finally
-            {
-                Dispose();
-            }
+            List<tblNews> news = await _context.tblNews.ToListAsync();
+            return View(news);
         }
 
-        [Route("Read/Blog/{id}")]
-        public async Task<IActionResult> Details(int id)
+        [Route("Read/News/{id}")]
+        public async Task<ActionResult> Details(int id)
         {
             try
             {
                 if (id > 0)
                 {
-                    tblBlogs data = await _context.tblBlogs.Where(x => x.BlogId == id).FirstOrDefaultAsync();
+                    tblNews data = await _context.tblNews.Where(x => x.NewsID == id).FirstOrDefaultAsync();
                     if (data != null)
                     {
                         return View(data);
                     }
                 }
-                return RedirectToAction("Error", "Home");
+                return RedirectToAction("Index");
             }
             catch (Exception e)
             {
                 Console.Write(e);
-                return RedirectToAction("Error", "Home");
+                return RedirectToAction("Index");
             }
         }
     }
